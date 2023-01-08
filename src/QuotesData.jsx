cf2f58c8adefe4d75e79dbbeff5c73798887848d
Yunus Dermawan <yunus.shevcenko@gmail.com>
2023-01-08 09:55:45 +0700

    Adding QuotesData.jsx and Randomizer.jsx

---
+++ src/QuotesData.jsx
@@ -0,0 +1,11 @@
+const quoteData = [
+    {text: "People often say that motivation doesn't last. Well, neither does bathing -- that's why we recommend it daily", author: "Zig Ziglar"}, 
+    {text: "Someday is not a day of the week.", author: "Denise Brennan-Nelson"}, 
+    {text: "Hire character. Train skill.", author: "Peter Schutz"},
+    {text: "Your time is limited, so don't waste it living someone else's life.", author: "Steve Jobs"},
+    {text: "Sales are contingent upon the attitude of the salesman -- not the attitude of the prospect.", author: "W. Clement Stone"},
+    {text: "Everyone lives by selling something.", author: "Robert Louis Stevenson"},
+    {text: "If you are not taking care of your customer, your competitor will.", author: "Bob Hooey"},
+    {text: "The golden rule for every businessman is this: Put yourself in your customer's place.", author: "Orison Swett Marden"},
+    {text: "If you cannot do great things, do small things in a great way.", author: "Orison Swett Marden"}
+  ]
