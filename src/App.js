e1bcff79a776af24456a84d06c9d375d55a1df15
Yunus Dermawan <yunus.shevcenko@gmail.com>
2023-01-11 21:54:59 +0700

    Adding App.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import QuoteBox from "./QuoteBox";
+import { useState } from "react";
+import Randomizer from "./Randomizer";
 
 function App() {
+    const [quote, setQuote] = React.useState({<Randomizer />})
+
     return (
         <>
             <QuoteBox />
